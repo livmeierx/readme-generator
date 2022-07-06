d14b539aa119133ac7ebb682f5bad62da9dbc73a
livmeierx <liv.meier13@gmail.com>
2022-07-06 19:53:32 -0400

    updated function to create README

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,31 @@
 //Creation of a single function to populate README
 function generateMarkdown(answers) {
-  
+  return `
+  <h1 align="center">${answers.projectTitle}</h1>
+
+  ## Description
+  ${answers.description}
+
+  ## Installation
+  ${answers.installation}
+
+  ## Usage
+  ${answers.usage}
+
+  ## License
+  This applications is coverd by the ${answers.license} license.
+
+  ## Contributing
+  ${answers.contributing}
+
+  ## Tests
+  ${answers.tests}
+
+  ## Questions
+  Find me on GitHub: [${answers.username}](https://github.com/${answers.username})
+
+  Email me with questions: ${answers.email}
+  `;
 }
 
-module.exports = generateMarkdown;
+export default generateMarkdown;
